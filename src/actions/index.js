13789cdef80a5c10880f3f2bd09bea4bfaf359f0
sunchenguang <sunchenguang@youzan.com>
2019-08-09 15:48:34 +0800

    add: init todo app

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import {ADD, DELETE} from '../constants/todo'
+
+export const add = (data) => {
+  return {
+    data,
+    type: ADD
+  }
+}
+
+export const del = (id) => {
+  return {
+    id,
+    type: DELETE
+  }
+}
+
+
+
+
+
+
+
+
